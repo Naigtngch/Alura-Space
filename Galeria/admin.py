12ad268e91a8c1f6cb994582ca7b622509a22959
Gian <gianlucassantos2310@gmail.com>
2024-11-10 17:45:58 -0300

    Atualização do Projeto

--- Galeria/admin.py
+++ Galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from Galeria.models import Fotografia
 
-# Register your models here.
+class listando_fotografias(admin.ModelAdmin):
+    list_display = ('nome', 'id', 'legenda', 'categoria', 'publicado')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_editable = ('publicado',)
+    list_filter = ('categoria',)
+
+admin.site.register(Fotografia, listando_fotografias)
